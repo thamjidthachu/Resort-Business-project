5f9984ff9c79fa7b84e45fc8a8cf92edcf02b9f1
Thamjid Thachu <91719380+thamjidthachu@users.noreply.github.com>
2022-05-11 10:29:44 +0530

    settings edit _ templates directory

--- Resort_Business_project/Resort_Business_project/settings.py
+++ Resort_Business_project/Resort_Business_project/settings.py
@@ -63,7 +63,8 @@ ROOT_URLCONF = 'Resort_Business_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Apps', 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, "templates"), os.path.join(BASE_DIR, "Apps")],
+        # 'DIRS': ['Apps', 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
